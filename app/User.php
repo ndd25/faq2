6157240ca239320e9ac7032a1a56316a74fe2a88
Nigum Dave <ndd25@njit.edu>
2019-05-10 22:43:42 -0400

    Added user profile migration model and updated the user seeder to create profiles

--- app/User.php
+++ app/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function profile()
+    {
+        return $this->hasone('App\Profile');
+    }
 }
