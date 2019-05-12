2a250fe98c581e78ececb0002ad763c35e744b48
Nigum Dave <ndd25@njit.edu>
2019-05-12 00:16:32 -0400

    Added Answer migrate, model, and made the answer seeder and added tests for user profile (cleanup), and answer test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -18,9 +18,21 @@ class UserTest extends TestCase
         $user = factory(\App\User::class)->make();
         $this->assertTrue($user->save());
     }
+
     public function testQuestions()
     {
         $user = factory(\App\User::class)->make();
         $this->assertTrue(is_object($user->questions()->get()));
+    }
+
+    public function testAnswers()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->answers()->get()));
+    }
+    public function testProfile()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->profile()->get()));
     }
 }
