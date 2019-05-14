eebaa7973ab2ba90b74162a078f3bab1f58d3882
Nigum Dave <ndd25@njit.edu>
2019-05-13 23:22:54 -0400

    Added delete button

--- app/Question.php
+++ app/Question.php
@@ -4,8 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Question extends Model
 {
+    protected $fillable = ['body'];
+
     public function user()
     {
         return $this->belongsTo('App\User');
